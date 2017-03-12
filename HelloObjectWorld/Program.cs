2eec8560172e2ed104f741272ba0a46be558c789
Pavel Barto <bartopavel@outlook.com>
2017-03-12 21:32:09 +0100

    Přidejte soubory projektu.

---
+++ HelloObjectWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloObjectWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Zdravic zdravic;
+            zdravic = new Zdravic();
+            zdravic.text = "Ahoj užívateľ ";
+            Console.WriteLine(zdravic.Pozdrav("Palo"));
+            Console.WriteLine(zdravic.Pozdrav("Karol"));
+            zdravic.text = "Vítam ťa programátor";
+            Console.WriteLine(zdravic.Pozdrav("Rišo"));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HelloObjectWorld/Zdravic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloObjectWorld
+{
+    class Zdravic
+    {
+        public string text;
+        public string Pozdrav(string meno)
+        {
+            // Console.WriteLine("Hello world objeckt ");
+            // Console.WriteLine("Ahoj užívateľ {0}", meno);
+            return String.Format("{0} {1}", text, meno);
+        }
+    }
+}
